296464fe207625aec34eaa7e97b5a285fe547236
walterstamm <walterstamm92@gmail.com>
2020-09-19 20:03:16 -0300

    En parte 3 see corrigio los errores para que compile

--- parte_3/Entrenamiento/configuracion.cpp
+++ parte_3/Entrenamiento/configuracion.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 #include <cstdlib>
+#include<stdio.h>
 #include "configuracion.h"
+#include "Usuarios.h"
 #include "ArchivosConfiguraciones.h"
 
 void RealizarCopiaSeguridad(){
@@ -15,13 +17,15 @@ void RealizarCopiaSeguridad(){
     int cant=TotalUsuarios();
     if(cant==-1){
         cout<<"-error de archivos 1-"<<endl;
+        system("pause");
         return;
     }
-    Usuarios *uno;
-    uno=(Usuarios *)malloc(cant * sizeof(Usuarios));
+    struct Usuarios *uno;
+    uno =( Usuarios *) malloc(cant * sizeof(Usuarios));
     if(uno==NULL){
         cout<<"-error de archivos 2-"<<endl;
         free(uno);
+        system("pause");
         return;
     }
     for(int x=0;x<cant;x++){
@@ -30,13 +34,16 @@ void RealizarCopiaSeguridad(){
     if(GuardamosCopiaUsuario(uno, cant)!=cant){
         cout<<"-error de archivos 3-"<<endl;
         free(uno);
+        system("pause");
         return;
     }
+
     free(uno);
     ///guardamos entrenamiento
     cant=TotalEntremanitos();
     if(cant==-1){
         cout<<"-error de archivos 1-"<<endl;
+        system("pause");
         return;
     }
     Entrenamiento*dos;
@@ -44,6 +51,7 @@ void RealizarCopiaSeguridad(){
     if(dos==NULL){
         cout<<"-error de archivos 2-"<<endl;
         free(dos);
+        system("pause");
         return;
     }
     for(int x=0;x<cant;x++){
@@ -52,6 +60,7 @@ void RealizarCopiaSeguridad(){
     if(GuardamosCopiaEntrenamiento(dos, cant)!=cant){
         cout<<"-error de archivos 3-"<<endl;
         free(dos);
+        system("pause");
         return;
     }
     free(dos);
