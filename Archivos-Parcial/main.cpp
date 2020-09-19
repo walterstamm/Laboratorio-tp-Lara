48d906dfe98a0bb546eb133eef0de2881570f748
walterstamm <walterstamm92@gmail.com>
2020-09-19 12:16:08 -0300

    Archivos Parcial
    Contiene los registros pedidos por Angel

---
+++ Archivos-Parcial/main.cpp
@@ -0,0 +1,25 @@
+//NOMBRE: HUGO FABIAN GOMEZ
+//TP N°: PRIMER PARCIAL
+
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <clocale>
+#include <iostream>
+using namespace std;
+#include "Usuarios.h"
+
+int main(){
+	setlocale(LC_ALL, "Spanish");
+    ///Usuarios reg;
+    ///Fecha dia;
+    Entrenamiento ent;
+    ///inicia el programa llamando al menu principal.
+    Menu();
+
+
+
+
+    return 0;
+}
